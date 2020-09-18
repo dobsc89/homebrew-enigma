546e317b914c095f7b88aaa0af14c356fdc00156
Carl Dobson <dobsc89@gmail.com>
2020-09-18 20:55:15 +0100

    Added Play 15

---
+++ Formula/play@15.rb
@@ -0,0 +1,30 @@
+class PlayAT14 < Formula
+  desc "Play’s goal is to ease Java web applications development."
+  homepage "https://www.playframework.com"
+  url "https://downloads.typesafe.com/play/1.5.3/play-1.5.3.zip"
+  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+
+  bottle :unneeded
+
+  conflicts_with "sox", :because => "Both install a `play` executable"
+  conflicts_with "play@12", :because => "Both install a `play` executable"
+
+  def install
+    rm_rf "python" # we don't need the bundled Python for windows
+    rm Dir["*.bat"]
+    libexec.install Dir["*"]
+    chmod 0755, libexec/"play"
+    bin.install_symlink libexec/"play"
+  end
+
+  test do
+    require "open3"
+    Open3.popen3("#{bin}/play new #{testpath}/app") do |stdin, _, _|
+      stdin.write "\n"
+      stdin.close
+    end
+    %w[app conf lib public test].each do |d|
+      File.directory? testpath/"app/#{d}"
+    end
+  end
+end
